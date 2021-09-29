b5fff8033c51bfaaf006fc5d5f0951b79dd206a8
brackets0 <brackets.web@gmail.com>
2021-09-30 01:23:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngx-markdown-vr8aeg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngx-markdown-vr8aeg)
---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <button class="btn-success" (click)="toggleMarkdown()">
+    <span *ngIf="!showMarkdown">Compile</span>
+    <span *ngIf="showMarkdown">Edit</span>
+  </button>
+  <button class="btn-primary" (click)="saveMarkdown()">SAVE</button>
+  <button class="btn-danger" (click)="clear()">CLEAR</button>
+
+  <div *ngIf="!showMarkdown" class="textWrapper">
+    <textarea [(ngModel)]="markdown"></textarea>
+  </div>
+
+  <div *ngIf="showMarkdown" class="markdownWrapper">
+    <markdown [data]="markdown"></markdown>
+  </div>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  showMarkdown = false;
+  markdown = ``;
+
+  ngOnInit() {
+    this.markdown = localStorage.getItem('markdown');
+  }
+
+  saveMarkdown() {
+    localStorage.setItem('markdown', this.markdown);
+  }
+  clear() {
+    this.markdown = '';
+  }
+  toggleMarkdown() {
+    if (this.showMarkdown) {
+      this.showMarkdown = false;
+    } else {
+      this.showMarkdown = true;
+    }
+  }
+}
